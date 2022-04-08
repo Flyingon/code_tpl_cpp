d9a4fba52d0e02d1be478ba55310d142ec870ab5
joeeyuan <joeeyuan@tencent.com>
2022-04-08 10:15:17 +0800

    update

--- for_mysql/mysql/connect.cpp
+++ for_mysql/mysql/connect.cpp
@@ -9,11 +9,10 @@ MYSQL mysql;
 
 // https://dev.mysql.com/doc/c-api/5.7/en/mysql-real-connect.html
 // https://downloads.mysql.com/archives/c-c/
+// 这里连接好像会根据mysql服务的ssl状态去觉得是否走ssl，现在不用走ssl
 void mysql_connect() {
-	mysql.options.use_ssl = false;
 	mysql_init(&mysql);
 	mysql_options(&mysql, MYSQL_READ_DEFAULT_GROUP, "your_prog_name");
-	mysql_options(&mysql, MYSQL_OPT_SSL_ENFORCE, false);
 	if (!mysql_real_connect(&mysql, 
 	 "1.13.5.232", "kbe", "pwd123456", "kbe", 3306, NULL, 0
 	))
