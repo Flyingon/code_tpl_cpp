a5114e52b987ae9dfcc43f5285b1a56eefb5e375
Flyingon <yuanzhaoyi@outlook.com>
2023-02-22 12:56:02 +0800

    update

---
+++ for_memory/for_size/redis.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+#include "stdio.h"
+#include <stdlib.h>
+
+using namespace std;
+
+#define REDIS_LRU_BITS 24
+#define REDIS_LRU_CLOCK_MAX ((1<<REDIS_LRU_BITS)-1) /* Max value of obj->lru */
+#define REDIS_LRU_CLOCK_RESOLUTION 1000 /* LRU clock resolution in ms */
+typedef struct redisObject {
+
+    // 类型
+    unsigned type: 4;
+
+    // 编码
+    unsigned encoding: 4;
+
+    // 对象最后一次被访问的时间
+    unsigned lru: REDIS_LRU_BITS; /* lru time (relative to server.lruclock) */
+
+    // 引用计数
+    int refcount;
+
+    // 指向实际值的指针
+    void *ptr;
+
+} robj;
+
+/* ZSETs use a specialized version of Skiplists */
+/*
+ * 跳跃表节点
+ */
+typedef struct zskiplistNode {
+
+    // 成员对象
+    robj *obj;
+
+    // 分值
+    double score;
+
+    // 后退指针
+    struct zskiplistNode *backward;
+
+    // 层
+    struct zskiplistLevel {
+
+        // 前进指针
+        struct zskiplistNode *forward;
+
+        // 跨度
+        unsigned int span;
+
+    } level[];
+
+} zskiplistNode;
+
+/*
+ * 跳跃表
+ */
+typedef struct zskiplist {
+
+    // 表头节点和表尾节点
+    struct zskiplistNode *header, *tail;
+
+    // 表中节点的数量
+    unsigned long length;
+
+    // 表中层数最大的节点的层数
+    int level;
+
+} zskiplist;
+
+
+/*
+ * 字典类型特定函数
+ */
+typedef struct dictType {
+
+    // 计算哈希值的函数
+    unsigned int (*hashFunction)(const void *key);
+
+    // 复制键的函数
+    void *(*keyDup)(void *privdata, const void *key);
+
+    // 复制值的函数
+    void *(*valDup)(void *privdata, const void *obj);
+
+    // 对比键的函数
+    int (*keyCompare)(void *privdata, const void *key1, const void *key2);
+
+    // 销毁键的函数
+    void (*keyDestructor)(void *privdata, void *key);
+
+    // 销毁值的函数
+    void (*valDestructor)(void *privdata, void *obj);
+
+} dictType;
+
+
+/*
+ * 哈希表节点
+ */
+typedef struct dictEntry {
+
+    // 键
+    void *key;
+
+    // 值
+    union {
+        void *val;
+        uint64_t u64;
+        int64_t s64;
+    } v;
+
+    // 指向下个哈希表节点，形成链表
+    struct dictEntry *next;
+
+} dictEntry;
+
+/* This is our hash table structure. Every dictionary has two of this as we
+ * implement incremental rehashing, for the old to the new table. */
+/*
+ * 哈希表
+ *
+ * 每个字典都使用两个哈希表，从而实现渐进式 rehash 。
+ */
+typedef struct dictht {
+
+    // 哈希表数组
+    dictEntry **table;
+
+    // 哈希表大小
+    unsigned long size;
+
+    // 哈希表大小掩码，用于计算索引值
+    // 总是等于 size - 1
+    unsigned long sizemask;
+
+    // 该哈希表已有节点的数量
+    unsigned long used;
+
+} dictht;
+
+/*
+ * 字典
+ */
+typedef struct dict {
+
+    // 类型特定函数
+    dictType *type;
+
+    // 私有数据
+    void *privdata;
+
+    // 哈希表
+    dictht ht[2];
+
+    // rehash 索引
+    // 当 rehash 不在进行时，值为 -1
+    int rehashidx; /* rehashing not in progress if rehashidx == -1 */
+
+    // 目前正在运行的安全迭代器的数量
+    int iterators; /* number of iterators currently running */
+
+} dict;
+
+/*
+ * 有序集合
+ */
+typedef struct zset {
+
+    // 字典，键为成员，值为分值
+    // 用于支持 O(1) 复杂度的按成员取分值操作
+    dict *dict;
+
+    // 跳跃表，按分值排序成员
+    // 用于支持平均复杂度为 O(log N) 的按分值定位成员操作
+    // 以及范围操作
+    zskiplist *zsl;
+
+} zset;
+
+int main() {
+    printf("sizof(zskiplist)=%d \n", sizeof(zskiplist));
+    printf("sizof(zset)=%d\n", sizeof(zset));
+
+
+    int key_num = 100;
+    printf("sizof(zskip)=%d \n", sizeof(zskiplist) +
+    sizeof(zskiplistNode)+32*sizeof(zskiplistNode::zskiplistLevel) +
+    sizeof(unsigned long) *  key_num);
+    printf("sizof(zset)=%d\n", sizeof(zset));
+}
---
+++ tookkit/gen_gcp_cmd/attach_download.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+const char cmd_attach_tpl[] = "kubectl exec -it %s -- /bin/bash";
+const char cmd_download_tpl[] = "kubectl cp %s:/deploy/%s /Users/zy.yuan/Job/temp/%s";
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("args is not enough");
+        return -1;
+    }
+    printf(cmd_attach_tpl, argv[1]);
+    printf("\n");
+    printf("cd /deploy");
+    printf("\n");
+    printf("tcpdump -XX -vvv -nn -i any -w %s", argv[2]);
+    printf("\n");
+    printf(cmd_download_tpl, argv[1], argv[2], argv[2]);
+    printf("\n");
+}
