5dc8f1d1ee5a8365481f7ad72cd4602c3d826203
yuanzhaoyi <yuanzhaoyi@outlook.com>
2023-03-26 15:59:32 +0800

    update

--- temp/temp.cpp
+++ temp/temp.cpp
@@ -4,17 +4,14 @@
 #include <string.h>
 #include <iostream>
 
-void c_strcpy(char *strDest, char *strSrc)
-{
-while( (*strDest++ = *strSrc++) != '\0' );
+void c_strcpy(char *strDest, char *strSrc) {
+    while ((*strDest++ = *strSrc++) != '\0');
 }
 
 int main(void) {
-    char* name = "Joeeyuan";
-    char name2[8] = {"J", "o", "e", "e", "y", "u", "a", "n"};
-    cout << name2 << std::end;
-
-    name[2] = "b";
-
+    char* name = "joeyuan";
+    name[2] = 'b';
+    std::cout << name << std::endl;
+    return 0;
 }
 
